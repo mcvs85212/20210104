165c1a33ca6ba294b88a81371046aa51c533466e
mcvs85212 <mcvs85212@mcvs.tp.edu.tw>
2021-01-04 14:28:46 +0800

    added 85212.py

--- 85212.py
+++ 85212.py
@@ -1,7 +1,17 @@
+def binary_search(data, left, right, target):
+    if left > right:                                        # 找不到
+        return -1    
+    middle = int((right-left) / 2 + left)                     # 取得left~right正中間的索引值(middle)    
+    if data[middle] > target:
+        return binary_search(data, left, middle-1, target)
+    elif data[middle] < target:
+        return binary_search(data, middle+1, right, target)
+        return middle
 data = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
 data_count = len(data)
 target = 16
 found_index = binary_search(data, 0, data_count-1, target)
+#-----------------
 if found_index == -1:
     print('無法找到搜尋目標', target)
 else:
